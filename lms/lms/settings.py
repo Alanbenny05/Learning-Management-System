7e961f58db780787695d08422a890e596a2f934a
alanbenny <alanbennynedumkattil05@gmail.com>
2026-02-13 12:47:14 +0530

    Settings.py updated.

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -123,4 +123,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_URL = 'accounts/login'
 LOGIN_REDIRECT_URL = '/dashboard/'
-LOGOUT_REDIRECT_URL = 'accounts/login'
+LOGOUT_REDIRECT_URL = 'accounts/login"
